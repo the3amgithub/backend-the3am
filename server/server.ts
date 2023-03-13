d38cfe13486c910be6c1712487002759ab6767a1
yash.codedrill <yash.codedrill@gmail.com>
2023-03-13 17:38:04 +0530

    add

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ const app: express.Application = express()
 
 const PORT = 8080
 const init = () => {
+  console.log(APp)
   app.listen(PORT, async () => {
     console.log(`App Is Running at  http://localhost:${PORT}`)
   })
