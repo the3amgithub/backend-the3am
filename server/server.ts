e62f94465955464c50b0eb08d3c46a865a0f2863
yash.codedrill <yash.codedrill@gmail.com>
2023-03-13 18:42:00 +0530

    add husky

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,8 @@ const app: express.Application = express()
 
 const PORT = 8080
 const init = () => {
-  console.log(APp)
   app.listen(PORT, async () => {
-    console.log(`App Is Running at  http://localhost:${PORT}`)
+    console.log(`App Is Running at http://localhost:${PORT}`)
   })
 }
 init()
